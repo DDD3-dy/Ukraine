0518679e20adb5ddb467299bb6bddcb5f4998d96
DyDyDev <dylan68@sfr.fr>
2022-02-27 12:00:42 +0100

    Initial Commit

---
+++ Ukraine/UkraineApp.swift
@@ -0,0 +1,17 @@
+//
+//  UkraineApp.swift
+//  Ukraine
+//
+//  Created by Dylan Caetano on 27/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct UkraineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
